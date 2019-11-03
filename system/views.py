b13e594ae5e7ed9741b8f2523b312e5246518e68
ago <ago@localhost.localdomain>
2019-11-03 14:02:23 -0500

    system home page

--- system/views.py
+++ system/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from landloard.models import LandlordProfile, HostelHomePage
 
 # Create your views here.
 
 
 def system_root_page(request):
-    context={}
+    universities = LandlordProfile.objects.all()
+    accommodation = HostelHomePage.objects.all()
+    context={
+        'universities': universities,
+        'accommodation': accommodation,
+    }
     return render(request, 'system_home.html', context)
