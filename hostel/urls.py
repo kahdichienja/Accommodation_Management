5f065ccf5ef2ddb18636589d91bbc466300be36e
ago <ago@localhost.localdomain>
2019-11-10 16:01:06 +0300

    settings 7

--- hostel/urls.py
+++ hostel/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('hostels/', include('tenants.urls')),
     path('user/', include('Profile.urls')),
     path('', include('system.urls')),
-     path('api/payments/', include('mpesa.api.urls')),
+    path('api/payments/', include('mpesa.api.urls')),
 ]
